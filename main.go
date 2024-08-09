e58078ca52a4f26eea3509d62fe5c1b1f1c961f9
fou3fou3 <foxedex29@gmail.com>
2024-08-09 19:01:46 +0100

    remove new planet from main

--- main.go
+++ main.go
@@ -2,22 +2,10 @@ package main
 
 import (
 	"github.com/CelestialCrafter/stella/db"
-	"github.com/CelestialCrafter/stella/planets"
 	"github.com/CelestialCrafter/stella/server"
-	"github.com/charmbracelet/log"
 )
 
 func main() {
-	planet := planets.NewPlanet(planets.PlanetFeatures{
-		Type:        planets.StarPlanet,
-		StarNeutron: true,
-	})
-
-	err := planet.CreateModel()
-	if err != nil {
-		log.Fatal("could not create model", "error", err)
-	}
-
 	db.InitDB()
 	defer db.CloseDB()
 
