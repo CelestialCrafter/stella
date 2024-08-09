354fc95d58e2bbf9a82696c75e8bacb97ea05c12
CelestialCrafter <celestialcraftermc@gmail.com>
2024-08-09 10:30:07 -0500

    select planets

--- server/web/src/App.svelte
+++ server/web/src/App.svelte
@@ -1,14 +1,19 @@
 <script>
 	import { onMount } from 'svelte';
-	import { initScene, addPlanets } from './scene.js';
+	import { initScene, addPlanets, selectedPlanet } from './scene.js';
 
 	let canvas;
 	onMount(() => initScene(canvas));
-	addPlanets(['/models/312a6ec69acf2c905f40c839af63873f7d2d3a3972f578b30d47f123e0b94932.glb', '/models/7d27a746d6abef020f1f33a9b3115399f45d8f145e6e4e1c1e95cab558d69ad0.glb', '/models/84203a979ef5729707d8f2fb2bc3e06fdcc76a7c1c04e4fa54d31977b0077c27.glb'])
+	addPlanets(['312a6ec69acf2c905f40c839af63873f7d2d3a3972f578b30d47f123e0b94932', '7d27a746d6abef020f1f33a9b3115399f45d8f145e6e4e1c1e95cab558d69ad0', '84203a979ef5729707d8f2fb2bc3e06fdcc76a7c1c04e4fa54d31977b0077c27'])
+
+	const handleCanvasClick = () => {
+		const selected = selectedPlanet();
+		selected != null && console.log(selected.name);
+	};
 </script>
 
 <main>
-	<canvas bind:this={canvas}></canvas> 
+	<canvas on:click={handleCanvasClick} bind:this={canvas}></canvas> 
 </main>
 
 <style>
--- server/web/src/scene.js
+++ server/web/src/scene.js
@@ -5,6 +5,8 @@ const FOV = 70;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(FOV, window.innerWidth / window.innerHeight, 0.1, 1000);
+const pointer = new THREE.Vector2();
+const raycaster = new THREE.Raycaster();
 const loader = new GLTFLoader();
 let renderer;
 let controls;
@@ -12,17 +14,30 @@ let controls;
 const light = new THREE.AmbientLight(0x404040, 50);
 scene.add(light);
 
+export const selectedPlanet = () => {
+  const intersects = raycaster.intersectObjects(Object.values(scene.children));
+
+  if (intersects.length > 0) {
+    for (const intersect of intersects) {
+      if (intersect.object.userData.name != "Sphere") {
+        continue
+      }
+
+      return intersect.object;
+    }
+  }
+
+  return null
+};
+
 const animate = () => {
   requestAnimationFrame(animate);
+
+  raycaster.setFromCamera(pointer, camera);
   controls.update();
   renderer.render(scene, camera);
 };
 
-const resize = () => {
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-};
 
 export const initScene = canvas => {
   renderer = new THREE.WebGLRenderer({
@@ -41,12 +56,13 @@ export const initScene = canvas => {
 };
 
 const load = url => new Promise((res, rej) => loader.load(url, res, () => {}, rej));
-export const addPlanets = async urls => {
-  const planets = (await Promise.all(urls.map(load))).map(gltf => gltf.scene);
+export const addPlanets = async hashes => {
+  const planetObjects = (await Promise.all(hashes.map(hash => `/models/${hash}.glb`).map(load))).map(gltf => gltf.scene);
 
   let totalBounding = new THREE.Box3();
-  for (const [i, planet] of planets.entries()) {
+  for (const [i, planet] of planetObjects.entries()) {
     scene.add(planet);
+    planet.children[0].name = hashes[i];
     const spaceVector = new THREE.Vector3(50, 0, 0)
     planet.position.add(spaceVector.multiplyScalar(i));
     totalBounding.expandByPoint(spaceVector);
@@ -57,4 +73,16 @@ export const addPlanets = async urls => {
   controls.target = center;
 };
 
+const pointerMove = event => {
+  pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+  pointer.y = - (event.clientY / window.innerHeight) * 2 + 1;
+};
+
+const resize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+};
+
+window.addEventListener('pointermove', pointerMove);
 window.addEventListener('resize', resize);
