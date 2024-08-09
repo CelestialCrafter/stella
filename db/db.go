4894b6bec0e2a86893c032aabf0f41f01672a0e5
fou3fou3 <foxedex29@gmail.com>
2024-08-09 14:49:06 +0100

    check hash existance & get planet by hash

--- db/db.go
+++ db/db.go
@@ -2,7 +2,6 @@ package db
 
 import (
 	"database/sql"
-
 	"os"
 
 	"github.com/charmbracelet/log"
@@ -11,20 +10,28 @@ import (
 
 const dbPath = "stella.db"
 
+type Planet struct {
+	Hash     string
+	Features int
+}
+
+var db *sql.DB
+
 func InitDB() {
+	var err error
+
 	if _, err := os.Stat(dbPath); os.IsNotExist(err) {
 		file, err := os.Create(dbPath)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatal("Failed to create database file:", err)
 		}
 		file.Close()
 	}
 
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err = sql.Open("sqlite3", dbPath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to open database:", err)
 	}
-	defer db.Close()
 
 	createTableSQL := `CREATE TABLE IF NOT EXISTS planets (
         hash TEXT PRIMARY KEY,
@@ -33,6 +40,37 @@ func InitDB() {
 
 	_, err = db.Exec(createTableSQL)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Failed to create table:", err)
 	}
+
+	log.Info("Database initialized successfully")
+}
+
+func CloseDB() {
+	if db != nil {
+		db.Close()
+		log.Info("Database connection closed")
+	}
+}
+
+func CheckHashExistance(hash string) bool {
+	var exists bool
+	db.QueryRow("SELECT 1 FROM planets WHERE hash = ? LIMIT 1", hash).Scan(&exists)
+
+	return exists
+}
+
+func GetPlanetByHash(hash string) (*Planet, error) {
+	var planet Planet
+
+	err := db.QueryRow("SELECT hash, features FROM planets WHERE hash = ?", hash).Scan(&planet.Hash, &planet.Features)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // No planet found with this hash
+		}
+		return nil, err // An error occurred during the query
+	}
+
+	return &planet, nil
 }
