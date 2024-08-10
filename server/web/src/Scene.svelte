dd2613771cfc9ab0a555a4c58a9a9eeaeb611aa5
CelestialCrafter <celestialcraftermc@gmail.com>
2024-08-10 13:51:35 -0500

    add selected planet

---
+++ server/web/src/Scene.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { onMount } from 'svelte';
+	import { initScene, addPlanets } from './scene.js';
+	export let handleCanvasClick;
+	export let planets;
+
+	let canvas;
+	onMount(() => {
+		addPlanets(Object.values(planets).map(planet => planet.hash));
+		initScene(canvas);
+	});
+</script>
+
+<canvas on:click={handleCanvasClick} bind:this={canvas}></canvas>
---
+++ server/web/src/SelectedPlanet.svelte
@@ -0,0 +1,32 @@
+<script>
+	export let planet;
+</script>
+
+{#if planet != null}
+	{@const { features, values } = planet}
+	<section class="selected-planet">
+		<span>Type: {features.type}</span><br />
+		<span>Neutron Star: {features.star_neutron ? 'yes' : 'no'}</span><br />
+
+		{#if features.type === 'normal'}
+			<span>Size: {values.normal_size}</span><br />
+			<span>Color: rgb {values.normal_color.join(', ')}</span><br />
+		{:else}
+			<span>Brightness: {values.star_brightness}</span><br />
+			<span>Size: {values.star_size}</span><br />
+			{#if features.star_neutron}
+				<span>Color: rgb {values.star_neutron_color.join(', ')}</span><br />
+			{/if}
+		{/if}
+	</section>
+{/if}
+
+<style>
+	.selected-planet {
+		width: 40%;
+		position: fixed;
+		background-color: white;
+		z-index: 10;
+		padding: 1rem;
+	}
+</style>
