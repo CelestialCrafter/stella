bae681c6da7d0a741ca50902233ae920fed8c96b
CelestialCrafter <celestialcraftermc@gmail.com>
2024-08-10 06:14:22 -0500

    organize server

--- server/routes.go
+++ server/routes.go
@@ -2,10 +2,35 @@ package server
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func Models(c echo.Context) error {
-	return c.String(http.StatusOK, "Up and working!")
+func svelte(g *echo.Group) {
+	svelteDevUrl, err := url.Parse(svelteDevAddress)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = http.Get(svelteDevAddress)
+	if err == nil {
+		g.Use(middleware.Proxy(middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{{
+			URL: svelteDevUrl,
+		}})))
+	} else {
+		g.Static("/", "server/web/dist")
+	}
+}
+
+func setupRoutes(e *echo.Echo) {
+	svelte(e.Group("/app"))
+	e.Static("/models", "models")
+
+	e.GET("/api/planet/:hash", GetPlanet)
+	e.GET("/api/planet/new", NewPlanet)
+	e.GET("/api/auth/login", Login)
+	e.GET("/api/auth/callback", Callback)
+
 }
--- server/server.go
+++ server/server.go
@@ -1,12 +1,8 @@
 package server
 
 import (
-	"net/http"
-	"net/url"
-
 	"github.com/charmbracelet/log"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 )
 
 const (
@@ -14,61 +10,15 @@ const (
 	svelteDevAddress = "http://localhost:5173"
 )
 
-func svelte(g *echo.Group) {
-	svelteDevUrl, err := url.Parse(svelteDevAddress)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = http.Get(svelteDevAddress)
-	if err == nil {
-		g.Use(middleware.Proxy(middleware.NewRoundRobinBalancer([]*middleware.ProxyTarget{{
-			URL: svelteDevUrl,
-		}})))
-	} else {
-		g.Static("/", "server/web/dist")
-	}
-}
-
-func logging(e *echo.Echo) {
-	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-		LogStatus:   true,
-		LogURI:      true,
-		LogError:    true,
-		HandleError: true,
-		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
-			if v.Error != nil {
-				log.Warn(
-					"request errored",
-					"uri", v.URI,
-					"code", v.Status,
-					"error", v.Error,
-				)
-				return nil
-			}
-
-			log.Info(
-				"request",
-				"uri", v.URI,
-				"code", v.Status,
-			)
-			return nil
-		},
-	}))
-}
-
 func SetupServer() {
 	e := echo.New()
 	e.HideBanner = true
 
 	logging(e)
-	svelte(e.Group("/app"))
-	e.Static("/models", "models")
+	setupRoutes(e)
 
-	e.GET("/api/planet/:hash", GetPlanet)
-	e.GET("/api/planet/new", NewPlanet)
-	e.GET("/api/auth/login", Login)
-	e.GET("/api/auth/callback", Callback)
-
-	e.Logger.Fatal(e.Start(bindAddress))
+	err := e.Start(bindAddress)
+	if err != nil {
+		log.Fatal("error starting server", "error", err)
+	}
 }
