ac12b6d22929f1408a688f29ea8c28c584a4bd7b
CelestialCrafter <celestialcraftermc@gmail.com>
2024-08-25 15:04:58 -0500

    Revert "change selected planet behaviour"
    This reverts commit db29d80f1226a625019df58d8db33931646a2d20.

--- server/web/src/planets/Scene.svelte
+++ server/web/src/planets/Scene.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { onDestroy, onMount } from 'svelte';
 	import { initScene } from './scene.js';
-	import { orbit, planets, selectedPlanet } from '../stores.js';
+	import { orbit, selectedPlanet } from '../stores.js';
 
 	let canvas;
 	let getSelectedPlanet = () => null;
@@ -32,12 +32,7 @@
 </div>
 
 <canvas
-	on:mousedown={() => {
-		const newSelected = getSelectedPlanet()?.name;
-		const planetHashes = Object.keys($planets);
-		if (!newSelected) return planetHashes.length > 0 ? selectedPlanet.set(planetHashes[0]) : null;
-		selectedPlanet.set(newSelected);
-	}}
+	on:mousedown={() => selectedPlanet.set(getSelectedPlanet()?.name || null)}
 	bind:this={canvas}
 ></canvas>
 
