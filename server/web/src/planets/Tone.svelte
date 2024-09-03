d1f9b68223c6874b9fd383458bb74bf88f90c2b5
CelestialCrafter <celestialcraftermc@gmail.com>
2024-09-03 10:48:00 -0500

    formatting

--- server/web/src/planets/Tone.svelte
+++ server/web/src/planets/Tone.svelte
@@ -1,28 +1,28 @@
 <script>
-export let hash;
-
-let audio;
-let volume;
-let paused = true;
+	export let hash;
+	
+	let audio;
+	let volume;
+	let paused = true;
 </script>
 
 <audio
-src={`/sounds/${hash}.wav`}
-bind:volume
-bind:this={audio}
-bind:paused
-loop
+	src={`/sounds/${hash}.wav`}
+	bind:volume
+	bind:this={audio}
+	bind:paused
+	loop
 ></audio>
 
 <div>
-<button on:click={() => paused ? audio.play() : audio.pause()}>
-{paused ? 'Play' : 'Pause'}
-</button>
-<input type="range" min="0" max="1" bind:value={volume} step="0.01">
+	<button on:click={() => paused ? audio.play() : audio.pause()}>
+		{paused ? 'Play' : 'Pause'}
+	</button>
+	<input type="range" min="0" max="1" bind:value={volume} step="0.01">
 </div>
 
 <style>
-input {
-width: 20%;
-}
+	input {
+		width: 20%;
+	}
 </style>
